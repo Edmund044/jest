89642ff751960ad4d1d0dafc7355ccc9ac47583f
Edmund044 <edmundopiyo@gmail.com>
2020-12-18 15:31:59 +0300

    Create util.js

---
+++ util.js
@@ -0,0 +1,35 @@
+exports.generateText = (name, age) => {
+    // Returns output text
+    return `${name} (${age} years old)`;
+    //return 'Max (29 years old)';
+  };
+  
+  exports.createElement = (type, text, className) => {
+    // Creates a new HTML element and returns it
+    const newElement = document.createElement(type);
+    newElement.classList.add(className);
+    newElement.textContent = text;
+    return newElement;
+  };
+  
+  exports.validateInput = (text, notEmpty, isNumber) => {
+    // Validate user input with two pre-defined rules
+    if (!text) {
+      return false;
+    }
+    if (notEmpty && text.trim().length === 0) {
+      return false;
+    }
+    if (isNumber && +text === NaN) {
+      return false;
+    }
+    return true;
+  };
+  exports.checkAndGenerate = (name, age) => {
+    if (
+        !validateInput(name, true, false) || !validateInput(age, false, true)
+    ){
+        return;
+    }
+    return generateText(name,age);
+  };
